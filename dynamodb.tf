07a3571765acdc263b347bbead62f09b0219cae8
Jafar Bohra <135355308+JafarBohra@users.noreply.github.com>
2024-09-10 09:01:46 +0530

    Delete dynamodb.tf

--- dynamodb.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_dynamodb_table" "name" {
-  name = var.aws_dynamodb_table
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key = "id"
-  attribute {
-    name = "id"
-    type = "S"
-  }
-}
